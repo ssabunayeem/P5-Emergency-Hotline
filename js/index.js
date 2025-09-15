23de893690c167066fc4f24681456d3c67e32446
ssabunayeem <ssabunayeem@gmail.com>
2025-09-16 05:30:40 +0600

    increase heart count done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
-// -----------increase heart--------
+// -----------------------reusable  function------------------------
+
+function getNumber(id) {
+    const data = document.getElementById(id).innerText;
+    return parseInt(data);
+}
+
+function getText(id) {
+    return document.getElementById(id).innerText;
+}
+
+
+
+
+// --------------------------increase heart-----------------------------
+
+const heartList = document.getElementsByClassName("heart-btn")
+
+
+for (const h of heartList) {
+    h.addEventListener("click", function () {
+        const heart = getNumber("heart");
+        const newValue = heart + 1;
+
+        console.log(newValue);
+        document.getElementById("heart").innerText = newValue;
+    })
+
+}
+
